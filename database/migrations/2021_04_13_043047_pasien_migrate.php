e8e81583d6aea1290ce202b153c7419c9a6e4fa1
yrartz <yasminramadini29@gmail.com>
2021-04-26 09:22:45 +0700

    membuat folder sistem informasi rumah sakit

---
+++ database/migrations/2021_04_13_043047_pasien_migrate.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class PasienMigrate extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+    Schema::create('pasien', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('ruang');
+            $table->string('telepon');
+            $table->text('alamat');
+            $table->dateTime('tanggal_lahir');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('pasien');
+    }
+}
